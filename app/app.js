ea879d0c8606d0c8b2e66d3e1a93ca945dc6812f
Ron Egli <ron@r-egli.com>
2021-10-13 23:23:07 -0700

    Updated formatting across the board

--- app/app.js
+++ app/app.js
@@ -8,6 +8,7 @@ const options = { type: 'application/json'}
 require('dotenv').config()
 var config = require('./config/config');
 var parser = require('./lib/parser.js');
+const fs = require('fs').promises;
 
 if(config.app.env != "PROD"){
     config.public.path = config.public.dev_directory;
@@ -19,38 +20,89 @@ if(config.app.env != "PROD"){
 app.use(bodyParser.raw(options));
 
 app.get('/', async function(req, res, next){
-    res.sendFile(config.public.path + 'index.html');
+    let body = await renderFile("header.html") + await renderFile("home.html") + await renderFile("footer.html");
+    res.status(200).send(body);
 });
 
 app.get('/examples', async function(req,res,next){
     res.status(200).send(JSON.parse(config.app.example_links));
-})
+});
+
+app.get('/favicon.ico', async function(req,res,next){
+    res.status(200).send("");
+});
+
+app.get('/testing', async function(req,res,next){
+    console.log("/testing called");
+    let body = await renderHeader();
+    body += "<p>" + "Hello World" + "</p>";
+    body += await renderFooter();
+
+
+    res.status(200).send(body);
+});
 
 app.get('/:url', async function(req, res, next){
+    let url = req.params.url;
+    console.log(url);
+
+    if(!url.startsWith("https://www.businessinsider.com") && !url.startsWith("https://www.wsj.com")){
+        //res.sendFile(config.public.path + 'badurl.html');
+        let body = await renderHeader();
+        body += await renderFile("badurl.html");
+        body += await renderFooter();
+
+        res.status(200).send(body);
+        return;
+    }
+
+    let body = await renderHeader();
+    body += await renderDisplay();
+    body += await renderFooter();
+
+    res.status(200).send(body);
+});
+
+app.get('/puller/:url', async function(req, res, next){
 
     let url = req.params.url;
 
     if(!url.startsWith("https://www.businessinsider.com") && !url.startsWith("https://www.wsj.com")){
-        res.status(404).send({error: "FQDN must match https://www.businessinsider.com or https://www.wsj.com"});
+        res.sendFile(config.public.path + 'badurl.html');
     }
 
     let content = await parser.parseIt(url);
 
-    content = "<p>" + content.join("</p><p>") + "</p>";
+    if(Array.isArray(content)){
 
-    if(content){
-        res.status(200).send(content);
-    }else{
-        res.sendFile('./public/index.html');
+        if(content){
+            res.status(200).send(content);
+        }else{
+            res.status(404).send("Invalid URL or Server Error");
+        }
     }
 
-    //let err = "404 - Endpoint not found or incorrect method used"
-	//res.status(404).send({ "message": "fail", "details": `${err}`, url: url });
 });
+
+let renderHeader = async function(){
+    return await fs.readFile(config.public.path + 'static/' + "header.html", 'utf8' );
+}
+
+let renderFooter = async function(){
+    return await fs.readFile(config.public.path + 'static/' + "footer.html", 'utf8' );
+}
+
+let renderDisplay = async function(){
+    return await fs.readFile(config.public.path + 'static/' + "display.html", 'utf8' );
+}
+
+let renderFile = async function(file){
+    return await fs.readFile(config.public.path + 'static/' + file, 'utf8' );
+}
 
 // Start the webserver on the specified by .env port
 app.listen(config.app.port, '0.0.0.0', () => {
-    console.log(`Example app listening at http://localhost:${config.app.port}`)
+    console.log(`Example app listening at http://0.0.0.0:${config.app.port}`)
   })
 
 
--- app/lib/parser.js
+++ app/lib/parser.js
@@ -7,35 +7,42 @@ module.exports = class parser {
 	static async parseIt(url){
 		//console.log(getFormattedDate() + ': Initiating Scrape');
         // Launch the browser
-        const browser = await puppeteer.launch({args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox'
-        ]});
-        // Open a new tab
-        const page = await browser.newPage();
-
-        await page.setExtraHTTPHeaders({
-            'user-agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
-            'upgrade-insecure-requests': '1',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'en-US,en;q=0.9,en;q=0.8',
-            'referer': "https://t.co/",
-            'X-Forwarded-For': '66.102.0.0'
-        })
-
-        await page.setCacheEnabled(false);
-        // Go to epic_url
-        await page.goto(url);
-
-        console.log(url)
+        try {
+            const browser = await puppeteer.launch({args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox'
+            ]});
+            // Open a new tab
+            const page = await browser.newPage();
+    
+            await page.setExtraHTTPHeaders({
+                'user-agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+                'upgrade-insecure-requests': '1',
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'en-US,en;q=0.9,en;q=0.8',
+                'referer': "https://t.co/",
+                'X-Forwarded-For': '66.102.0.0'
+            })
+    
+            await page.setCacheEnabled(false);
+            // Go to epic_url
+            await page.goto(url);
+    
+            console.log(url)
+            
+            const data = await page.$$eval("p", elements=> elements.map(item=>item.textContent))
+    
+            console.log(data);
+    
+            browser.close();
+    
+            return data;
+        }
+        catch(e){
+            console.log(e);
+            return false;
+        }
         
-        const data = await page.$$eval("p", elements=> elements.map(item=>item.textContent))
-
-        console.log(data);
-
-        browser.close();
-
-        return data;
     }
 };
